509497a22a434cce920606ad44967c02df245518
Julian Schoepf <ferlojulian@hotmail.com>
2022-01-22 16:32:26 +0100

    Get all memes to show in localhost

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  // create state to hold templates
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.memegen.link/templates').then((x) =>
+      x.json().then((response) => setTemplates(response)),
+    );
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      {templates.map((template) => {
+        return (
+          <img
+            style={{ width: 200 }}
+            key={template.id}
+            src={template.blank}
+            alt={template.name}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
